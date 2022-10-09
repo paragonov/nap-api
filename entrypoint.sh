0978cef1a96c5bafa4a304517856e29e62ba69ce
vladimir <vladimirhoris@gmail.com>
2022-10-09 13:43:59 +0700

    create superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 python manage.py makemigrations
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
 
 exec "$@"
