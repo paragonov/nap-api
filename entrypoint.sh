af579432c5ec8dba550eacbcfa9b8edd87e82fbf
vladimir <vladimirhoris@gmail.com>
2022-10-09 13:56:31 +0700

    delete

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-python manage.py makemigrations
-python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
-
-exec "$@"
