2b23873f62514edd3de7393ba8c3714de6222e4c
vladimir <vladimirhoris@gmail.com>
2022-10-09 13:48:31 +0700

    correct admin.py

--- api/newsapp/admin.py
+++ api/newsapp/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from .models import News, Positions, Insites, Trends
 
 
+class NewsAdmin(admin.ModelAdmin):
+    fields = ('title', 'date', 'positions')
 
-admin.site.register(News)
-admin.site.register(Positions)
+
+class PositionsAdmin(admin.ModelAdmin):
+    fields = ('name', 'slug',)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(NewsAdmin)
+admin.site.register(PositionsAdmin)
 admin.site.register(Insites)
 admin.site.register(Trends)
